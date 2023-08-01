f1045ab23e5123a67ed177e5e025fbfd6882f2ab
Kinjalk <kinjalkbajpai@gmail.com>
2023-08-01 22:45:04 +0530

    Able to serve resource related to both languages and topics

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const TelegramBot = require("node-telegram-bot-api")
 require('dotenv').config()
-const scrapy = require("./scrapy")
+const languages = require("./scrapeByLanguages")
+const topics = require("./scrapeByTopics")
+let secondMenu = false
 
 const token = process.env.BOT_KEY
+const MAX_MESSAGE_LENGTH = 4096;
 
 const bot = new TelegramBot(token, { polling: true })
 
 let menuTriggered = false
 let allOptions = []
+
+function splitMessage(message) {
+    const chunks = [];
+    let currentChunk = '';
+
+    const words = message.split(' ');
+
+    for (const word of words) {
+        if (currentChunk.length + word.length + 1 <= MAX_MESSAGE_LENGTH) {
+            currentChunk += word + ' ';
+        } else {
+            chunks.push(currentChunk.trim());
+            currentChunk = word + ' ';
+        }
+    }
+
+    if (currentChunk.trim() !== '') {
+        chunks.push(currentChunk.trim());
+    }
+
+    return chunks;
+}
+
 
 bot.onText(/\/start/, (msg) => {
 
@@ -24,33 +50,73 @@ bot.onText(/\/start/, (msg) => {
 // });
 
 bot.onText(/\/m/, async (msg) => {
-    const data = await scrapy()
-    menuTriggered = true
-    let buttons = []
-    allOptions = data;
-    for (let i = 1; i < data.length; i++) {
-        buttons.push([data[i].headingText])
-    }
+
     bot.sendMessage(msg.chat.id, "Menu", {
         "reply_markup": {
-            "keyboard": buttons
+            "keyboard": [["Programming Languages"], ["Topics"]]
         }
     })
+
+    menuTriggered = true
+
 })
 
 bot.on('message', async (msg, match) => {
     const chatId = msg.chat.id;
-    if (menuTriggered) {
+    if (menuTriggered && msg.text === "Programming Languages") {
+        const data = await languages()
+        let buttons = []
+        allOptions = data;
+        for (let i = 1; i < data.length; i++) {
+            buttons.push([data[i].headingText])
+        }
+        bot.sendMessage(msg.chat.id, "Menu", {
+            "reply_markup": {
+                "keyboard": buttons
+            }
+        })
+        secondMenu = true
+        menuTriggered = false
+    }
+    else if (menuTriggered && msg.text === "Topics") {
+        const data = await topics()
+        let buttons = []
+        allOptions = data;
+        for (let i = 1; i < data.length; i++) {
+            buttons.push([data[i].headingText])
+        }
+        bot.sendMessage(msg.chat.id, "Menu", {
+            "reply_markup": {
+                "keyboard": buttons
+            }
+        })
+        secondMenu = true
+        menuTriggered = false
+    }
+    else if (secondMenu) {
+        console.log("Test")
+        console.log(msg.text)
         for (let i = 0; i < allOptions.length; i++) {
+
             if (msg.text === allOptions[i].headingText) {
-                let message = `Here are the resources for ${msg.text}:\n`
+                console.log("Hello worl")
+                let message = `Here are the resources for ${msg.text}:\n\n`
                 allOptions[i].listItems.forEach((link, index) => {
-                    message += `${index + 1}. ${link.text} ${link.href}\n`
+                    message += `${index + 1}. ${link.text} ${link.href}\n\n`
                 });
-                bot.sendMessage(chatId, message)
-                // console.log(allOptions[i].listItems)
+                if (message.length > 4095) {
+                    const messageChunks = splitMessage(message);
+                    for (const chunk of messageChunks) {
+                        bot.sendMessage(chatId, chunk);
+                    }
+                }
+                else {
+                    bot.sendMessage(chatId, message)
+                }
+
+                //console.log(allOptions[i].listItems)
             }
         }
-        menuTriggered = false
+        secondMenu = false
     }
 })
---
+++ scrapeByTopics.js
@@ -0,0 +1,61 @@
+const puppeteer = require("puppeteer")
+const fs = require('fs');
+
+const scrapeByTopics = async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null
+    });
+
+    const page = await browser.newPage();
+
+    await page.setJavaScriptEnabled(true);
+
+
+    await page.goto("https://github.com/EbookFoundation/free-programming-books/blob/main/books/free-programming-books-subjects.md", {
+        waitUntil: "domcontentloaded"
+    })
+
+    const links = await page.evaluate(() => {
+        const section = document.querySelector('.entry-content')
+        const ulElements = section.querySelectorAll('ul');
+        const associations = [];
+
+        ulElements.forEach((ul, index) => {
+            let heading = ul.previousElementSibling;
+            while (heading && !['H3', 'H4'].includes(heading.tagName)) {
+                heading = heading.previousElementSibling;
+            }
+
+            if (heading) {
+                const listItems = Array.from(ul.querySelectorAll('li')).map(li => {
+                    const anchor = li.querySelector('a');
+                    return {
+                        href: anchor ? anchor.getAttribute('href') : null,
+                        text: anchor ? anchor.innerText.trim() : li.innerText.trim(),
+                    };
+                });
+
+                associations.push({
+                    headingText: heading.innerText.trim(),
+                    ulIndex: index,
+                    listItems,
+                });
+            }
+
+        });
+
+        return associations;
+
+    });
+    // console.log(links); // Output the extracted associations (optional).
+
+    await browser.close()
+
+    return links
+
+}
+
+// scrapy()
+
+module.exports = scrapeByTopics
